d3f3e504f0375a90ae1f81910d7d81fdeaf0a09d
CluelessD3v <cluelessdevmail@gmail.com>
2023-08-14 13:54:46 -0500

    add assets

--- src/Server/Main.server.lua
+++ src/Server/Main.server.lua
@@ -1,63 +1,6 @@
 local RunService = game:GetService("RunService")
 local Sorbet = require(game.ReplicatedStorage.Sorbet)
 
--- local Person = {
--- 	Age = 0,
--- }
-
--- local Born = Sorbet.State({
--- 	Name = "Born",
--- 	Enter = function(entity, fsm)
--- 		print("Entered")
--- 		entity.Age += 1
--- 		entity.Birthday = os.date()
--- 		fsm:ChangeState(entity, "Aging")
--- 	end,
-
--- 	Update = function(entity, fsm)
--- 	end,
-
--- 	Exit = function(entity, fsm)
--- 		print "Bye, have a good life"
--- 	end,
--- } :: Sorbet.State)
-
-
--- local ageInterval = 10
--- local ageTS
--- local Aging = Sorbet.State({
--- 	Name = "Aging",
--- 	Enter = function(entity, fsm)
--- 		ageTS = os.clock()
--- 		print("Daym son, you now aging!")
--- 	end,
-
--- 	Update = function(entity, fsm, dt)
--- 		if ageTS >= ageInterval then
--- 			entity.Age  += 1
--- 			print("happy birthday!", entity.Age)		
--- 		end
--- 		-- fsm:ChangeState(entity, "Aging")
--- 	end,
-
--- 	Exit = function(entity, fsm)
--- 	end,
--- } :: Sorbet.State)
-
-
--- local myLife = Sorbet.FSM({
--- 	Entities = {Person},
--- 	States = {Born, Aging},
--- 	InitialState = Born
--- })
-
--- myLife:AddEntity(Person)
--- myLife:StartEntity(Person)
--- -- myLife:StopEntity(Person)
-
-
--- myLife:Update()
-
 local idleTimestampts = {}
 local idleTime = 3
 local Idle = Sorbet.State({
